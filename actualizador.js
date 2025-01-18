1fcd165104ffedd28ec0d69691171e0c732241f2
gam-buena-vista <gambv1710@gmail.com>
2025-01-17 22:24:47 -0400

    Add files via upload

---
+++ actualizador.js
@@ -0,0 +1,5 @@
+{
+    
+	document.getElementById("fecha_cabecera").innerHTML = " Ultima actualizacion Lunes 9 de Enero de 2025 ";
+		document.getElementById("fecha_pie").innerHTML = " Sitio actualizado al: 09/01/2025 ";
+} 
